c5c7f60363ad296ff5a2fa146833aae3a8f553ae
Bahadır <oxikairos@hotmail.com>
2023-02-04 03:03:12 +0300

    Digging sound effect is added

--- Sport/Assets/Scripts/Dig.cs
+++ Sport/Assets/Scripts/Dig.cs
@@ -6,6 +6,7 @@ public class Dig : MonoBehaviour
 {
     [SerializeField] private GameObject shovel;
     [SerializeField] private GameObject dirt;
+    [SerializeField] private AudioSource diggingSound;
     
     void Start()
     {
@@ -19,6 +20,20 @@ public class Dig : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(ShovelDig());
+    }
+
+    IEnumerator ShovelDig()
+    {
+        DigSound();
+
+        yield return new WaitForSeconds(diggingSound.clip.length - 0.6f);
+
         dirt.SetActive(true);
+    }
+
+    private void DigSound()
+    {
+        diggingSound.Play();
     }
 }
