ca14d429039e63125fb6d0fac0e14e8e53c1dc55
Bahadır <oxikairos@hotmail.com>
2023-03-31 14:10:35 +0300

    Objects can be created by voice

--- Sport/Assets/Scripts/WitAI Scripts/CreateByVoice.cs
+++ Sport/Assets/Scripts/WitAI Scripts/CreateByVoice.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class CreateByVoice : MonoBehaviour
 {
-    [SerializeField] GameObject carrot;
+    //Objects to create
+    [SerializeField] GameObject tomato;
+    [SerializeField] GameObject eggplant;
+    [SerializeField] GameObject corn;
+    [SerializeField] GameObject chair;
+
+    //Create location
+    [SerializeField] Transform createPosition;
 
     public void CreateCommand(string[] values)
     {
         Debug.Log("Create Manager: " + "Object: " + values[0]);
+
+        switch (values[0])
+        {
+            case "tomato":
+                CreateObject(tomato);
+                break;
+            case "eggplant":
+                CreateObject(eggplant);
+                break;
+            case "corn":
+                CreateObject(corn);
+                break;
+            case "chair":
+                CreateObject(chair);
+                break;
+        }
+    }
+
+    void CreateObject(GameObject objectToCreate)
+    {
+        GameObject obj = Instantiate(objectToCreate, createPosition.position, Quaternion.identity);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+
+        rb.isKinematic = false;
+        rb.useGravity = true;
     }
 }
